1707a75ef26050ec859c697543b2ff82d868a4b4
hanlord <chen007@live.at>
2022-08-09 14:32:32 +0200

    update links

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/', name: 'app_test')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        ]);
+    }
+    #[Route('/test/{id}', name: 'app_test_link')]
+    public function profile(ManagerRegistry $doctrine, $id): Response
+    {
+        $user = $doctrine->getRepository(User::class)->find($id);
+        return $this->render('test/link.html.twig', [
+            'user' => $user,
         ]);
     }
 }
