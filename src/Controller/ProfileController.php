becbc7fcc0c87e791b4c2bc1ef7fd6c724a1f1d9
hanlord <chen007@live.at>
2022-08-09 15:44:11 +0200

    up foreign key

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Address;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +16,10 @@ class ProfileController extends AbstractController
     public function profileIndex(ManagerRegistry $doctrine, $id): Response
     {
         $user = $doctrine->getRepository(User::class)->find($id);
+        $fkaddress = $user->getFkAddress();
         return $this->render('profile/index.html.twig', [
             'user' => $user,
+            'address' => $fkaddress,
         ]);
     }
     #[Route('/profile/{id}/edit', name: 'app_profile_edit')]
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_test');
+            return $this->redirectToRoute('app_test_profile');
         }
 
         // get the login error if there is one
