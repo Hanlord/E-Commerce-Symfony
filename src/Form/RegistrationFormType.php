6b54b1ff67b66ec90f45f7000c961ebaa3aef929
hanlord <chen007@live.at>
2022-08-09 10:54:55 +0200

    up regis form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,15 +20,6 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -46,14 +37,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('fk_address', EntityType::class, [
-                'class' => Address::class,
-                'choice_label' => 'name',
-            ])
+            
             ->add('name')
             ->add('surname')
             ->add('image')
-
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
             
         ;
     }
