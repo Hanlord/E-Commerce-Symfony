9717603f01422fd7b89d0b6c4e8c4c383d9174b9
hanlord <chen007@live.at>
2022-08-10 14:34:51 +0200

    add cart files

---
+++ src/Controller/ShoppingCartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShoppingCartController extends AbstractController
+{
+    #[Route('/shopping/cart', name: 'app_shopping_cart')]
+    public function index(): Response
+    {
+        return $this->render('shopping_cart/index.html.twig', [
+            'controller_name' => 'ShoppingCartController',
+        ]);
+    }
+}
