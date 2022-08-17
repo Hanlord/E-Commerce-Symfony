293f0dec1e33299a0722f7d460125b3dcf520c88
Maria Zakharova <zakharovamary@gmail.com>
2022-08-17 19:33:38 +0200

    styling for discount and category + particles

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,14 +6,22 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label_attr'=>['class'=>'form-label'],
+                'row_attr'=>['class'=>'mb-2'],
+                'attr' => ['class' => 'form-control mb-1'] ])  
+            ->add('description', TextareaType::class, [
+                'label_attr'=>['class'=>'form-label'],
+                'row_attr'=>['class'=>'mb-1'],
+                'attr' => ['class' => 'form-control mb-1'] ])
         ;
     }
 
--- src/Form/DiscountType.php
+++ src/Form/DiscountType.php
@@ -6,15 +6,26 @@ use App\Entity\Discount;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DiscountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('discountType')
-            ->add('amount')
+            ->add('name', TextType::class, [
+                'label_attr'=>['class'=>'form-label'],
+                'row_attr'=>['class'=>'mb-2'],
+                'attr' => ['class' => 'form-control mb-1'] ])
+            ->add('discountType', TextType::class, [
+                'label_attr'=>['class'=>'form-label'],
+                'row_attr'=>['class'=>'mb-2'],
+                'attr' => ['class' => 'form-control mb-1'] ])
+            ->add('amount', IntegerType::class, [
+                'label_attr'=>['class'=>'form-label'],
+                'row_attr'=>['class'=>'mb-2'],
+                'attr' => ['class' => 'form-control mb-1'] ]) 
         ;
     }
 
